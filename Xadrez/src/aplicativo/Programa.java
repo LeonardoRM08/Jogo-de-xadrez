98a988eb370ec0ddc36cb0dc888f6af69e74e374
Leonardo <leonardomarangao@hotmail.com>
2023-10-09 17:56:48 -0300

    criada primeira classe: Posição

---
+++ Xadrez/src/aplicativo/Programa.java
@@ -0,0 +1,11 @@
+package aplicativo;
+
+import tabuleiro.Posicão;
+
+public class Programa {
+    public static void main(String[] args) {
+        Posicão p = new Posicão(3, 5);
+
+        System.out.println(p);
+    }
+}
